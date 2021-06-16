2e2aba06b4a488467ecd62e4705ce45b5c41e96c
Zhentian Kai <38963539+ZKai0801@users.noreply.github.com>
2021-06-16 13:42:44 +0800

    record original file paths

--- trash.sh
+++ trash.sh
@@ -7,12 +7,14 @@ TRANSH_DIR="$HOME/.trash"
 if [[ ! -d $TRANSH_DIR  ]];
 then
     mkdir $TRANSH_DIR
+    touch $TRANSH_DIR/filepaths
 fi
 
 for i in $*;
 do
     stamp=`date +%y%m%d.%H%M`
     filename=`basename $i`
-
+    
+    echo "$stamp.$filename $(realpath $i)" >> $TRANSH_DIR/filepaths
     mv $i $TRANSH_DIR/$stamp.$filename
 done
