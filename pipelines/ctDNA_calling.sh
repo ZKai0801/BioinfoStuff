f1e9bfcf0087f98d8b4df5001aeff07e2f6c652c
Zhentian Kai <38963539+ZKai0801@users.noreply.github.com>
2023-01-17 14:50:34 +0800

    update qc & variant filtering

--- pipelines/ctDNA_calling.sh
+++ pipelines/ctDNA_calling.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/bash
 
-version="v1.1"
+version="v1.2"
 
 # ----------------------------------- Description ------------------------------------- #
 # Perform:                                                                              #
@@ -23,7 +23,8 @@ version="v1.1"
 # ---------------------------------------------------------------------- #
 
 mode="single"
-umi_templ="6M+T,6M+T"
+umi_templ="4M+T,4M+T"
+# umi_templ="6M1S+T,6M1S+T"
 
 # path to software & scripts
 fastp="/data/ngs/softs/fastp/fastp"
@@ -38,9 +39,10 @@ VIC="/public/software/VIC"
 annovar="/public/software/annovar/"
 varscan="/public/software/varscan/VarScan.v2.4.2.jar"
 
-merge_mnv="/public/home/kai/BioinfoStuff/tertiary_analysis/merge_mnv.py"
-anno_hgvs="/public/home/kai/BioinfoStuff/tertiary_analysis/anno_hgvs.py"
-rm_common_variant="/public/home/kai/BioinfoStuff/rm_common_variant.py"
+merge_mnv="/public/software/BioinfoStuff/tertiary_analysis/merge_mnv.py"
+anno_hgvs="/public/software/BioinfoStuff/tertiary_analysis/anno_hgvs.py"
+rm_common_variant="/public/software/BioinfoStuff/rm_common_variant.py"
+hotspot_filt="/public/test_data/capp_seq/hotspot_filter.py"
 
 sentieon_license="172.16.11.242:8991"
 thread=8
@@ -54,20 +56,21 @@ k3="/data/ngs/database/soft_database/GATK_Resource_Bundle/hg19/Mills_and_1000G_g
 refflat="/public/database/GATK_Resource_Bundle/refFlat.txt"
 vep_dir="/public/software/vep_98/"
 cache_version="98"
-clinic_transcripts="/public/home/kai/database/LRG/parsed_LRG.tsv"
-
+#clinic_transcripts="/public/home/kai/database/LRG/parsed_LRG.tsv"
+clinic_transcripts="/public/database/LRG/parsed_LRG.tsv"
 
 
 # switch (on||off)
-# do_trim="on"
-# do_align="on"
-# do_qc="on"
-# do_realign="on"
-# do_tnscope="on"
-# do_mpileup="on"
-# do_varscan="on"
+do_trim="on"
+do_align="on"
+do_qc="on"
+do_realign="on"
+do_tnscope="on"
+do_mpileup="on"
+do_varscan="on"
 do_filt="on"
-# do_anno="on"
+do_anno="on"
+do_filt2="on"
 
 
 # ------------------------------ argparser ----------------------------- #
@@ -84,7 +87,7 @@ fi
 input_folder=`realpath $1`
 output_folder=`realpath $2`
 
-if [[  -f $bed  ]];
+if [[  -f $3  ]];
 then
     bed=`realpath $3`
 else
@@ -150,10 +153,9 @@ mean_depth,mean_dedup_depth,dup_rate(%),\
 average_insert_size,std_insert_size,\
 Uniformity_0.1X(%),Uniformity_0.2X(%),\
 Uniformity_0.5X(%),Uniformity_1X(%),\
-50x_depth_percent(%),100x_depth_percent(%),\
-150x_depth_percent(%),200x_depth_percent(%),\
-300x_depth_percent(%),400x_depth_percent(%),\
-500x_depth_percent(%)" \
+1000x_depth_percent(%),2000x_depth_percent(%),\
+3000x_depth_percent(%),4000x_depth_percent(%),\
+5000x_depth_percent(%)" \
 > $qc_dir/QC_summary.csv
 
 
@@ -218,15 +220,8 @@ function run_alignment {
 # 3. qc
 function run_qc {
     echo "LOGGING: ${sampleID} -- `date --rfc-3339=seconds` -- qc";
-
-    if [[ ! -d $qc_dir/${sampleID} ]]; then
-        mkdir $qc_dir/${sampleID};
-    fi;
     
-    if [[  $bed != "empty"  ]];
-    then
-        $bamdst -p $bed -o $qc_dir/${sampleID} $bam;
-    fi
+    bam=${align_dir}/${sampleID}.sorted.bam
 
     $samtools stats -@ ${thread} $bam > ${qc_dir}/${sampleID}.stats.txt;
 
@@ -253,52 +248,75 @@ function run_qc {
     fh = json.load(open('$trim_dir/${sampleID}.trim.json', 'r')); \
     print(round(fh['summary']['before_filtering']['q30_rate']*100, 2))"`
 
+    local trim_rate=`python3 -c "print(round(100-$clean_bases/$raw_bases*100, 2))"`
+
     local insert_size=$(awk -F"\t" '$2 == "insert size average:" {print $3}' ${qc_dir}/${sampleID}.stats.txt);
     local insert_std=$(awk -F"\t" '$2 == "insert size standard deviation:" {print $3}' ${qc_dir}/${sampleID}.stats.txt);
 
+
     if [[  $bed != "empty"  ]];
     then
+        if [[ ! -d $qc_dir/${sampleID} ]]; then
+            mkdir $qc_dir/${sampleID};
+        fi;
+
+        $bamdst -p $bed -o $qc_dir/${sampleID} $bam;
+
         local mapping_rate=$(grep "Fraction of Mapped Reads" $qc_dir/${sampleID}/coverage.report | awk -F"\t" '{print $2}');
         local mean_depth=$(grep "Average depth" $qc_dir/${sampleID}/coverage.report |head -n 1 |awk -F"\t" '{print $2}');
         local mean_dedup_depth=$(grep "Average depth(rmdup)" $qc_dir/${sampleID}/coverage.report |head -n 1 |awk -F"\t" '{print $2}');
         local dup_rate=$(grep "Fraction of PCR duplicate reads" $qc_dir/${sampleID}/coverage.report |awk -F"\t" '{print $2}');
         local on_target=$(grep "Fraction of Target Reads in all reads" $qc_dir/${sampleID}/coverage.report |awk -F"\t" '{print $2}');
 
-        local cov_50x=$(less -S $qc_dir/${sampleID}/depth.tsv.gz | awk 'BEGIN {count=0} {if ($4 > 50) count+=1} END {print count/NR*100}');
-        local cov_100x=$(less -S $qc_dir/${sampleID}/depth.tsv.gz | awk 'BEGIN {count=0} {if ($4 > 100) count+=1} END {print count/NR*100}');
-        local cov_150x=$(less -S $qc_dir/${sampleID}/depth.tsv.gz | awk 'BEGIN {count=0} {if ($4 > 150) count+=1} END {print count/NR*100}');
-        local cov_200x=$(less -S $qc_dir/${sampleID}/depth.tsv.gz | awk 'BEGIN {count=0} {if ($4 > 200) count+=1} END {print count/NR*100}');
-        local cov_300x=$(less -S $qc_dir/${sampleID}/depth.tsv.gz | awk 'BEGIN {count=0} {if ($4 > 300) count+=1} END {print count/NR*100}');
-        local cov_400x=$(less -S $qc_dir/${sampleID}/depth.tsv.gz | awk 'BEGIN {count=0} {if ($4 > 400) count+=1} END {print count/NR*100}');
-        local cov_500x=$(less -S $qc_dir/${sampleID}/depth.tsv.gz | awk 'BEGIN {count=0} {if ($4 > 500) count+=1} END {print count/NR*100}');
+        local cov_1000x=$(less -S $qc_dir/${sampleID}/depth.tsv.gz | awk 'BEGIN {count=0} {if ($4 > 1000) count+=1} END {print count/NR*100}');
+        local cov_2000x=$(less -S $qc_dir/${sampleID}/depth.tsv.gz | awk 'BEGIN {count=0} {if ($4 > 2000) count+=1} END {print count/NR*100}');
+        local cov_3000x=$(less -S $qc_dir/${sampleID}/depth.tsv.gz | awk 'BEGIN {count=0} {if ($4 > 3000) count+=1} END {print count/NR*100}');
+        local cov_4000x=$(less -S $qc_dir/${sampleID}/depth.tsv.gz | awk 'BEGIN {count=0} {if ($4 > 4000) count+=1} END {print count/NR*100}');
+        local cov_5000x=$(less -S $qc_dir/${sampleID}/depth.tsv.gz | awk 'BEGIN {count=0} {if ($4 > 5000) count+=1} END {print count/NR*100}');
 
-        local uniformity_01x=$(less -S $qc_dir/${sampleID}/depth.tsv.gz | awk -v depth=${tumor_mean_dedup_depth} 'BEGIN {count=0} {if ($4 > depth*0.1) count+=1} END {print count/NR*100}');
-        local uniformity_02x=$(less -S $qc_dir/${sampleID}/depth.tsv.gz | awk -v depth=${tumor_mean_dedup_depth} 'BEGIN {count=0} {if ($4 > depth*0.2) count+=1} END {print count/NR*100}');
-        local uniformity_05x=$(less -S $qc_dir/${sampleID}/depth.tsv.gz | awk -v depth=${tumor_mean_dedup_depth} 'BEGIN {count=0} {if ($4 > depth*0.5) count+=1} END {print count/NR*100}');
-        local uniformity_1x=$(less -S $qc_dir/${sampleID}/depth.tsv.gz | awk -v depth=${tumor_mean_dedup_depth} 'BEGIN {count=0} {if ($4 > depth) count+=1} END {print count/NR*100}');
+        local uniformity_01x=$(less -S $qc_dir/${sampleID}/depth.tsv.gz | awk -v depth=${mean_dedup_depth} 'BEGIN {count=0} {if ($4 > depth*0.1) count+=1} END {print count/NR*100}');
+        local uniformity_02x=$(less -S $qc_dir/${sampleID}/depth.tsv.gz | awk -v depth=${mean_dedup_depth} 'BEGIN {count=0} {if ($4 > depth*0.2) count+=1} END {print count/NR*100}');
+        local uniformity_05x=$(less -S $qc_dir/${sampleID}/depth.tsv.gz | awk -v depth=${mean_dedup_depth} 'BEGIN {count=0} {if ($4 > depth*0.5) count+=1} END {print count/NR*100}');
+        local uniformity_1x=$(less -S $qc_dir/${sampleID}/depth.tsv.gz | awk -v depth=${mean_dedup_depth} 'BEGIN {count=0} {if ($4 > depth) count+=1} END {print count/NR*100}');
     else
         local mapping_rate=".";
         local mean_depth=".";
         local mean_dedup_depth=".";
         local dup_rate=".";
         local on_target=".";
-        local cov_50x=".";
-        local cov_100x=".";
-        local cov_150x=".";
-        local cov_200x=".";
-        local cov_300x=".";
-        local cov_400x=".";
-        local cov_500x=".";
+        local cov_1000x=".";
+        local cov_2000x=".";
+        local cov_3000x=".";
+        local cov_4000x=".";
+        local cov_5000x=".";
         local uniformity_01x=".";
         local uniformity_02x=".";
         local uniformity_05x=".";
         local uniformity_1x="."
     fi
 
+
+    if [[ -n $umi_templ  ]];
+    then
+        if [[ ! -d $qc_dir/${sampleID}_umi ]]; then
+            mkdir $qc_dir/${sampleID}_umi;
+        fi;
+
+        dedup_bam=${align_dir}/${sampleID}.sorted.dedup.bam
+
+        if [[  $bed != "empty"  ]];
+        then
+            $bamdst -p $bed -o $qc_dir/${sampleID}_umi $dedup_bam;
+            
+            local mean_dedup_depth=$(grep "Average depth" $qc_dir/${sampleID}_umi/coverage.report |head -n 1 |awk -F"\t" '{print $2}');
+            local dup_rate=`python3 -c "print(round(100-$mean_dedup_depth/$mean_depth*100, 2))"`
+        fi
+    fi
+
     echo "${sampleID},${r1}/${r2},${raw_reads},${raw_bases},${clean_reads},${clean_bases},\
-    ${qc_rate},${mapping_rate},${on_target},${mean_depth},${mean_dedup_depth},${dup_rate},${insert_size},${insert_std},\
+    ${qc_rate},${trim_rate},${mapping_rate},${on_target},${mean_depth},${mean_dedup_depth},${dup_rate},${insert_size},${insert_std},\
     ${uniformity_01x},${uniformity_02x},${uniformity_05x},${uniformity_1x},\
-    ${cov_50x},${cov_100x},${cov_150x},${cov_200x},${cov_300x},${cov_400x},${cov_500x}" \
+    ${cov_1000x},${cov_2000x},${cov_3000x},${cov_4000x},${cov_5000x}" \
     >> $qc_dir/QC_summary.csv
 }
 
@@ -359,7 +377,37 @@ function run_tnscope {
 }
 
 
-# 6. generate mpileup
+# 6. filter variants
+function filter_vars {
+    echo "LOGGING: $sampleID -- `date --rfc-3339=seconds` -- filter low quality variants";
+
+    grep -v "SVTYPE=BND" ${tnscope_dir}/${sampleID}.raw.vcf > ${tnscope_dir}/${sampleID}.step0_snv.vcf
+
+    # left normalisation
+    $bcftools norm -m -both -f ${ref} \
+    ${tnscope_dir}/${sampleID}.step0_snv.vcf \
+    -o ${tnscope_dir}/${sampleID}.step1_norm.vcf;
+
+    $bcftools annotate -x "FILTER/triallelic_site" ${tnscope_dir}/${sampleID}.step1_norm.vcf | \
+    $bcftools filter -m + -s "low_qual" -e "QUAL < 10" | \
+    $bcftools filter -m + -s "short_tandem_repeat" -e "RPA[0]>=5" | \
+    $bcftools filter -m + -s "read_pos_bias" -e "FMT/ReadPosRankSumPS[0] < -5" | \
+    $bcftools filter -m + -s "base_qual_bias" -e "FMT/BaseQRankSumPS[0] < -5" | \
+    $sentieon util vcfconvert - ${tnscope_dir}/${sampleID}.step2_pre_filt.vcf
+
+    # remove variants that non-pass & qual < 100
+    awk '$0 ~ /^#/ || ($6 > 100 && $7 == "PASS")' \
+    ${tnscope_dir}/${sampleID}.step2_pre_filt.vcf >\
+    ${tnscope_dir}/${sampleID}.step3_filt.vcf;
+
+    $bcftools filter -i "(FORMAT/AF[:0]) >= 0.0002 & (FORMAT/AD[:0]+AD[:1]) >= 1000" \
+    ${tnscope_dir}/${sampleID}.step3_filt.vcf \
+    -o ${tnscope_dir}/${sampleID}.step4_filt.vcf
+
+}
+
+
+# ?. generate mpileup
 function gen_mpileup {
     echo "LOGGING: ${sampleID} -- `date --rfc-3339=seconds` -- generate mpileup";
 
@@ -374,7 +422,7 @@ function gen_mpileup {
 }
 
 
-# 7. variant calling with Varscan
+# ?. variant calling with Varscan
 function run_varscan {
     echo "LOGGING: ${sampleID} -- `date --rfc-3339=seconds` -- variant calling (Varscan)";
     java -Xmx3550m -Xms3550m -Xmn2g -jar $varscan \
@@ -399,42 +447,29 @@ function run_varscan {
 }
 
 
-# 8. filter variants
-function filter_vars {
-    echo "LOGGING: ${sampleID} -- `date --rfc-3339=seconds` -- TNscope variant normalisation and filtering"
-    
-    # left normalisation
-    $bcftools norm -m -both -f ${ref} \
-    ${tnscope_dir}/${sampleID}.raw.vcf \
-    -o ${tnscope_dir}/${sampleID}.step1_norm.vcf;
-
-    # remove variants that non-pass & qual < 100
-    awk '$0 ~ /^#/ || ($6 > 100 && $7 == "PASS")' \
-    ${tnscope_dir}/${sampleID}.step1_norm.vcf >\
-    ${tnscope_dir}/${sampleID}.step2_filt.vcf;
-
-    $bcftools filter -i "(FORMAT/AF[:0]) >= 0.0002 & (FORMAT/AD[:0]+AD[:1]) >= 10000" \
-    ${tnscope_dir}/${sampleID}.step2_filt.vcf \
-    -o ${tnscope_dir}/${sampleID}.step3_filt.vcf
-
-}
-
-
-# 9. annotation
-function do_anno {
+# 7. annotation
+function anno_vars {
     echo "LOGGING: ${sampleID} -- `date --rfc-3339=seconds` -- VEP Annotation";
-
     $vep --dir ${vep_dir} --cache --offline --cache_version ${cache_version} \
     --assembly GRCh37 --format vcf --fa ${ref} --force_overwrite --vcf \
     --gene_phenotype --use_given_ref --refseq --check_existing \
     --hgvs --hgvsg --transcript_version --max_af \
-    --vcf_info_field ANN -i ${snv_dir}/${sampleID}.vcf \
-    -o ${snv_dir}/${sampleID}.step8_anno.vcf;
+    --vcf_info_field ANN -i ${tnscope_dir}/${sampleID}.step4_filt.vcf \
+    -o ${tnscope_dir}/${sampleID}.step5_anno.vcf;
 
-    python3 $anno_hgvs ${snv_dir}/${sampleID}.step8_anno.vcf \
-    $clinic_transcripts $refflat -o $snv_dir/$sampleID.step9_anno.vcf;
+    python3 $anno_hgvs ${tnscope_dir}/${sampleID}.step5_anno.vcf \
+    $clinic_transcripts $refflat -o $tnscope_dir/$sampleID.step6_anno.vcf;
 }
 
+
+# 8. keep hotspot variants only
+function filter_vars2 {
+    echo "LOGGING: ${sampleID} -- `date --rfc-3339=seconds` -- keep hotspot variants only";
+
+    # remove variants with max-MAF > 0.1% 
+    python3 $hotspot_filt -i $tnscope_dir/$sampleID.step6_anno.vcf > $tnscope_dir/$sampleID.step7_filt.vcf
+
+}
 
 
 
@@ -520,58 +555,19 @@ if [[  $mode == 'matched' ]]; then
         # step7 - normalisation + remove low quality variants
         if [[  $do_filt == "on"  ]];
         then
-            echo "LOGGING: ${sampleID} -- `date --rfc-3339=seconds` -- normalise VCF + filter low-support";
-            
-            # remove SV
-            grep -v "SVTYPE=BND" ${snv_dir}/${sampleID}.raw.vcf \
-            >  ${snv_dir}/${sampleID}.step1_snv.vcf;
-
-            # split multiallelic sites + left-alignment
-            $bcftools norm -m -both -f ${ref} \
-            ${snv_dir}/${sampleID}.step1_snv.vcf \
-            -o ${snv_dir}/${sampleID}.step2_norm.vcf;
-
-            # bgzip and make index file
-            $bgzip ${snv_dir}/${sampleID}.step2_norm.vcf;
-            $tabix -p vcf ${snv_dir}/${sampleID}.step2_norm.vcf.gz;
-
-            # filter off-target variants
-            $bcftools view -R $bed \
-            ${snv_dir}/${sampleID}.step2_norm.vcf.gz \
-            > ${snv_dir}/${sampleID}.step3_on_target.vcf;
-
-            # filter low-support variants
-            awk '($1 ~ /^#/) || ($7 ~ /PASS/)' ${snv_dir}/${sampleID}.step3_on_target.vcf > \
-            ${snv_dir}/${sampleID}.step4_filter.vcf
-
-            $bcftools filter -i "(FORMAT/AF[:0]) >= 0.0002" \
-            ${snv_dir}/${sampleID}.step4_filter.vcf > \
-            ${snv_dir}/${sampleID}.step5_filter.vcf;
-
-            $bcftools filter -i "(FORMAT/AD[:0]+AD[:1]) >= 1000" \
-            ${snv_dir}/${sampleID}.step5_filter.vcf > \
-            ${snv_dir}/${sampleID}.step6_filter.vcf; 
-
-            # merge MNV
-            python3 $merge_mnv ${snv_dir}/${sampleID}.step6_filter.vcf ${ref} \
-            -o $snv_dir/${sampleID}.step7_MNV_merged.vcf;
+            filter_vars;
         fi
 
 
         # step8 - annotation
         if [[  $do_anno == "on"  ]];
         then
-            echo "LOGGING: ${sampleID} -- `date --rfc-3339=seconds` -- VEP Annotation";
+            anno_vars;
+        fi
 
-            $vep --dir ${vep_dir} --cache --offline --cache_version ${cache_version} \
-            --assembly GRCh37 --format vcf --fa ${ref} --force_overwrite --vcf \
-            --gene_phenotype --use_given_ref --refseq --check_existing \
-            --hgvs --hgvsg --transcript_version --max_af \
-            --vcf_info_field ANN -i ${snv_dir}/${sampleID}.step7_MNV_merged.vcf \
-            -o ${snv_dir}/${sampleID}.step8_anno.vcf;
-
-            python3 $anno_hgvs ${snv_dir}/${sampleID}.step8_anno.vcf \
-            $clinic_transcripts $refflat -o $snv_dir/$sampleID.step9_anno.vcf;
+        if [[  $do_filt2 == "on"  ]];
+        then
+            filter_vars2;
         fi
     done
 
@@ -590,13 +586,6 @@ elif [[  $mode == 'single'  ]]; then
         if [[ $do_align == "on" ]];
         then
             run_alignment;
-        fi
-
-        if [[  -n $umi_templ  ]];
-        then
-            bam=${align_dir}/${sampleID}.sorted.dedup.bam
-        else
-            bam=${align_dir}/${sampleID}.sorted.bam
         fi
 
         # step5 - quality control
@@ -635,67 +624,15 @@ elif [[  $mode == 'single'  ]]; then
             filter_vars;
         fi
 
-        
-        # # step8 - normalise & remove low-quality variants
-        # if [[ $do_filt == "on"  ]];
-        # then
-        #     echo "LOGGING: ${sampleID} -- `date --rfc-3339=seconds` -- normalise VCF + filter low-support";
+        if [[  $do_anno == "on"  ]];
+        then
+            anno_vars;
+        fi
 
-        #     # remove 'germline_risk' annotation
-        #     $bcftools annotate -x FILTER/germline_risk \
-        #     ${snv_dir}/${sampleID}.tumor.raw.vcf > \
-        #     ${snv_dir}/${sampleID}.step1_deanno.vcf;
-
-        #     # remove SV
-        #     grep -v "SVTYPE=BND" ${snv_dir}/${sampleID}.step1_deanno.vcf \
-        #     >  ${snv_dir}/${sampleID}.step2_snv.vcf;
-
-        #     # split multiallelic sites + left-alignment
-        #     $bcftools norm -m -both -f ${ref} \
-        #     ${snv_dir}/${sampleID}.step2_snv.vcf \
-        #     -o ${snv_dir}/${sampleID}.step3_norm.vcf;
-
-        #     # bgzip and make index file
-        #     $bgzip ${snv_dir}/${sampleID}.step3_norm.vcf;
-        #     $tabix -p vcf ${snv_dir}/${sampleID}.step3_norm.vcf.gz;
-
-        #     # filter off-target variants
-        #     $bcftools view -R $bed \
-        #     ${snv_dir}/${sampleID}.step3_norm.vcf.gz \
-        #     > ${snv_dir}/${sampleID}.step4_on_target.vcf;
-
-        #     # filter low-support variants
-        #     grep "#\|PASS" ${snv_dir}/${sampleID}.step4_on_target.vcf > \
-        #     ${snv_dir}/${sampleID}.step5_filter.vcf
-
-        #     $bcftools filter -i "(FORMAT/AF[:0]) >= 0.05" \
-        #     ${snv_dir}/${sampleID}.step5_filter.vcf > \
-        #     ${snv_dir}/${sampleID}.step6_filter.vcf;
-
-        #     $bcftools filter -i "(FORMAT/AD[:0]+AD[:1]) >= 250" \
-        #     ${snv_dir}/${sampleID}.step6_filter.vcf > \
-        #     ${snv_dir}/${sampleID}.step7_filter.vcf; 
-
-        #     # merge MNVs
-        #     python3 $merge_mnv ${snv_dir}/${sampleID}.step6_filter.vcf ${ref} \
-        #     -o $snv_dir/${sampleID}.step7_MNV_merged.vcf;
-        # fi
-
-        # # step9 - annotation
-        # if [[  $do_anno == "on"  ]];
-        # then
-        #     echo "LOGGING: ${sampleID} -- `date --rfc-3339=seconds` -- VEP Annotation";
-
-        #     $vep --dir ${vep_dir} --cache --offline --cache_version ${cache_version} \
-        #     --assembly GRCh37 --format vcf --fa ${ref} --force_overwrite --vcf \
-        #     --gene_phenotype --use_given_ref --refseq --check_existing \
-        #     --hgvs --hgvsg --transcript_version --max_af \
-        #     --vcf_info_field ANN -i ${snv_dir}/${sampleID}.step7_MNV_merged.vcf \
-        #     -o ${snv_dir}/${sampleID}.step8_anno.vcf;
-
-        #     python3 $anno_hgvs ${snv_dir}/${sampleID}.step8_anno.vcf \
-        #     $clinic_transcripts $refflat -o $snv_dir/$sampleID.step9_anno.vcf;
-        # fi
+        if [[  $do_filt2 == "on"  ]];
+        then
+            filter_vars2;
+        fi
     done
 fi
 
